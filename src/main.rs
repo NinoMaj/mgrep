8562a45af10610b823fd457a473520b6898f283a
Nino Majder <nino.majder@gmail.com>
2021-02-05 23:08:40 +0100

    Add search logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+use std::process;
+
+use mgrep;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = mgrep::Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = mgrep::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
